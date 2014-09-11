6efc907bc4482d64063a9ee3576468745ed22fb6
sindhu_nk <sindhunkashyap@gmail.com>
2014-09-10 23:46:27 -0700

    Hello world bootRun-scala-gradle app

--- src/main/scala/hello/HelloConfig.scala
+++ src/main/scala/hello/HelloConfig.scala
@@ -1,18 +1,24 @@
 package hello
-
 import org.springframework.context.annotation.Configuration
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
-
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.stereotype.Controller
 /**
- * This config class will trigger Spring @annotation scanning and auto configure Spring context.
- *
- * @author saung
- * @since 1.0
- */
+* This config class will trigger Spring @annotation scanning and auto configure Spring context.
+*
+* @author saung
+* @since 1.0
+*/
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@Controller
 class HelloConfig {
-
+	@RequestMapping(value = Array("/"))
+	@ResponseBody
+	def functionName(): String = {
+		return "Hello World!";
+	}
 }
